1b1eb333093f01f24cbf3fbba5143693393a31c6
施熠民 SHI YIMIN <117010230@link.cuhk.edu.cn>
2025-01-10 14:23:22 +0800

    run2.sh

--- run2.sh
+++ run2.sh
@@ -1,12 +1,15 @@
-for lr in 0.01 0.005 0.001 0.0005
+for lr in 0.02 0.01 0.005
 do
-    for lamda in 0.1 0.05 0.02 0.01
+    for lamda in 0.02 0.01 0.005
     do
         for layer in 1 2
         do
             for batch in 10000 40000
             do
-                python3 run_exp.py --model 6  --lr $lr --lamda $lamda --layer $layer --batch $batch
+                for alpha in 1e-4
+                do
+                    python3 run_exp.py --model 14 --lr $lr --lamda $lamda --layer $layer --batch $batch --epoch 300 --afd_alpha $alpha
+                done
             done
         done
     done
